8bef8b3c9104f9e32f7d0cdc72f0f7a9f1defc82
KimLuanDev <Troll.Jayd@gmail.com>
2026-01-08 06:05:03 +0700

    upload

--- public/profile.js
+++ public/profile.js
@@ -99,7 +99,7 @@ avatarInput.onchange = async () => {
   if (!file) return;
 
   // ⚠️ Giới hạn size để tránh localStorage quá lớn
-  if (file.size > 300 * 1024) {
+  if (file.size > 1250 * 1250) {
     alert("❌ Ảnh quá lớn (tối đa 300KB)");
     return;
   }
