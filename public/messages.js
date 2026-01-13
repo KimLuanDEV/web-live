6ac7474b861913d6824c68cd0e94fce4bd6406c7
KimLuanDev <Troll.Jayd@gmail.com>
2026-01-14 02:19:15 +0700

    up

--- public/messages.js
+++ public/messages.js
@@ -1,69 +1,27 @@
 const socket = io();
 const auth = JSON.parse(localStorage.getItem("user_profile") || "{}");
 
-let currentTarget = null;
-
 socket.emit("auth-login", { uid: auth.uid });
 
 const userList = document.getElementById("userList");
-const chatBox = document.getElementById("chatBox");
-const chatTitle = document.getElementById("chatTitle");
 
 socket.on("active-users", ({ users }) => {
   userList.innerHTML = "";
 
   users.forEach(u => {
-    if(u.name === auth.name) return;
+    if (u.uid === auth.uid) return;
 
-  const div = document.createElement("div");
-div.className = "user-item";
-div.innerHTML = `
-  <img class="user-avatar" src="${u.avatar}">
-  <div class="user-name">${u.name}</div>
-`;
+    const div = document.createElement("div");
+    div.className = "inbox-item";
+    div.innerHTML = `
+      <img class="inbox-avatar" src="${u.avatar}">
+      <div class="inbox-name">${u.name}</div>
+    `;
 
-div.onclick = () => {
-  location.href = "/chat.html?uid=" + u.uid;
-};
-
-
-
+    div.onclick = () => {
+      location.href = "/chat.html?uid=" + u.uid;
+    };
 
     userList.appendChild(div);
   });
 });
-
-
-socket.on("private-chat-history", (list) => {
-  chatBox.innerHTML = "";
-  list.forEach(m => {
-    const name = m.from.uid === auth.uid ? "Bạn" : m.from.name;
-    pushMsg(name, m.text);
-  });
-});
-
-document.getElementById("sendBtn").onclick = () => {
-  const txt = document.getElementById("msgInput").value.trim();
-  if(!txt || !currentTarget) return;
-
-  socket.emit("private-message", {
-    to: currentTarget.socketId,
-    text: txt
-  });
-
-  pushMsg("Bạn", txt);
-  document.getElementById("msgInput").value = "";
-};
-
-socket.on("private-message", ({ from, text }) => {
-  pushMsg(from.name, text);
-});
-
-function pushMsg(name, text){
-  const isMe = name === "Bạn";
-  const div = document.createElement("div");
-  div.className = "msg " + (isMe ? "me" : "other");
-  div.textContent = text;
-  chatBox.appendChild(div);
-  chatBox.scrollTop = chatBox.scrollHeight;
-}
