5e8f0af61840217bca6d0a47b027d83d5d86d36c
KimLuanDev <Troll.Jayd@gmail.com>
2026-01-15 16:05:36 +0700

    up

--- public/guard.js
+++ public/guard.js
@@ -2,3 +2,15 @@ const p = localStorage.getItem("user_profile");
 if(!p){
   location.href="/login.html";
 }
+
+window.addEventListener("load", ()=>{
+  const auth = JSON.parse(localStorage.getItem("user_profile") || "{}");
+
+  if(auth?.uid && window.socket){
+    socket.emit("auth-ping", { uid: auth.uid });
+
+    setInterval(()=>{
+      socket.emit("auth-ping", { uid: auth.uid });
+    }, 15000);
+  }
+});
