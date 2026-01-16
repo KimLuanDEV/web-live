c3bbf375a1869e70c9d242f9365d357d1688d967
KimLuanDev <Troll.Jayd@gmail.com>
2026-01-16 23:47:29 +0700

    up

--- server.js
+++ server.js
@@ -531,11 +531,7 @@ const active = [...safeMap(room.viewerProfiles).values()]
 
    const vc = safeMap(room.viewerProfiles).size;
   const br = calcBitrate(vc);
-  
- if(room.broadcasterId){
   io.to(roomId).emit("set-bitrate", { bitrate: br });
-}
-
 }
 
 
