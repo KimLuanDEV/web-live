4b7461a205f5531d0fcbe5606837499c60f011ad
KimLuanDev <Troll.Jayd@gmail.com>
2026-01-21 13:52:36 +0700

    up

--- public/messages.js
+++ public/messages.js
@@ -316,8 +316,8 @@ function pushMsg(name, text, isMe=false, msgId=null, status="", avatar="") {
   // 💬 TEXT → GIỮ BUBBLE
   else {
    html = `
-  <div ${isMe ? "me" : "other"}">
-    <span class="bubble-text">${text}</span>
+  <div class="bubble-text" ${isMe ? "me" : "other"}">
+  ${text}
   </div>
 `;
   }
