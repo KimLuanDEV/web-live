927c717d677face5731b3a3276f7ef5db2a4f738
KimLuanDev <Troll.Jayd@gmail.com>
2026-01-27 19:33:36 +0700

    up

--- public/withdraw.js
+++ public/withdraw.js
@@ -2,7 +2,7 @@ const params = new URLSearchParams(location.search);
 const received = Number(params.get("received") || 0);
 
 // 🔁 TỶ GIÁ (bạn chỉnh tùy ý)
-const RATE = 0.95; // 1 kim cương = 100đ / giá đô $
+const RATE = 950; // 1 kim cương = 100đ / giá đô $
 // ⛔ RÚT TỐI THIỂU
 const MIN_WITHDRAW = 5000; // 💎 tối thiểu để được rút
 
