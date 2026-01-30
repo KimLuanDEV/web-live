faccbfc91892c59060c014368bae10ceeb609582
KimLuanDev <Troll.Jayd@gmail.com>
2026-01-30 16:24:41 +0700

    up

--- public/invest.js
+++ public/invest.js
@@ -5,8 +5,6 @@
 
 const me = JSON.parse(localStorage.getItem("user_profile") || "{}");
 
-const coinEl = document.getElementById("myCoin");
-if (coinEl) coinEl.textContent = me.coins || 0;
 
 function goInvest(asset){
   location.href = `/invest-detail.html?asset=${asset}`;
