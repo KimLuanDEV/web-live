5369a22524920d179c7f519f40c147fc182e5476
KimLuanDev <Troll.Jayd@gmail.com>
2026-02-06 23:16:23 +0700

    up

--- public/game-wheel.js
+++ public/game-wheel.js
@@ -71,7 +71,7 @@ const angle  = Math.atan2(dy, dx) * 180 / Math.PI;
 
 // 🎯 CHỈ CHẠM MÉP Ô
 const slotRadius = slotRect.width / 2; // 64px / 2 = 32
-const gap = 6;                         // khoảng hở cho đẹp
+const gap = 3;                         // khoảng hở cho đẹp
 
 const finalLength = Math.max(0, length - slotRadius - gap);
 
