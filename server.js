59e4924e04a25d814c51de0715b0478e110a0f45
KimLuanDev <Troll.Jayd@gmail.com>
2026-03-18 23:09:30 +0700

    up

--- server.js
+++ server.js
@@ -1980,7 +1980,7 @@ function fakeWin(realSlot){
   const slot = WHEEL8_SLOTS[realSlot-1]
   const m = slot.m
 
-  const betList = [50,100,200,500,1000]
+  const betList = [500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000]
 
   let bet =
     betList[Math.floor(Math.random()*betList.length)]
