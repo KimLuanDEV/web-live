c67be1d0970c9ac603a0aa177430d4716becdda4
KimLuanDev <Troll.Jayd@gmail.com>
2026-03-20 18:13:53 +0700

    up

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ function getTimeRaceBetList(){
     const id = "bot_"+i
 
     list[id] = {
-      name: "???" + (1000 + Math.floor(Math.random()*9000)),
+      name: "???",
       total: Math.floor(Math.random()*5000 + 500)
     }
   }
