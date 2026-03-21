a17ff617d3cd3ec8e0814ef03ec94af484c5d798
KimLuanDev <Troll.Jayd@gmail.com>
2026-03-21 16:29:42 +0700

    up

--- public/admin.js
+++ public/admin.js
@@ -1234,3 +1234,23 @@ function showForceLogoutModal(message){
 
 
 
+
+
+document.addEventListener("gesturestart", function(e){
+  e.preventDefault();
+});
+
+let lastTouchEnd = 0;
+
+document.addEventListener("touchend", function(e){
+
+  const now = Date.now();
+
+  if(now - lastTouchEnd <= 300){
+    e.preventDefault();
+  }
+
+  lastTouchEnd = now;
+
+}, false);
+
