30a013ea307e936c720910a8ead1d42a5d33c6e8
KimLuanDev <Troll.Jayd@gmail.com>
2026-03-28 16:57:20 +0700

    up

--- server.js
+++ server.js
@@ -6569,16 +6569,7 @@ app.get("/admin-time-race.html",(req,res)=>{
 });
 
 
-app.get("/api/deposit-history",(req,res)=>{
 
-const history = loadDepositHistory()
-
-res.json({
-success:true,
-history
-})
-
-})
 
 
 app.get("/api/user-info",(req,res)=>{
